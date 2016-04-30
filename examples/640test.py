b4a9998ca67c404ce89bc94d0ac8f3af27121853
shrkey <barry@shrkey.com>
2016-04-30 21:08:32 +0100

    shorten test

--- examples/640test.py
+++ examples/640test.py
@@ -5,17 +5,17 @@ dw = dw_MotorCONTROL( addr=0x60 )
 m = dw.getMotor(1)
 
 m.run(dw_MotorCONTROL.RELEASE)
-time.sleep(5)
+time.sleep(1)
 
 ##time.sleep(10)
 print "Set forward"
 m.setMotorSpeed(255)
-time.sleep(5)
+time.sleep(1)
 print "stop"
 m.setMotorSpeed(0)
-time.sleep(5)
+time.sleep(1)
 print "Set reverse"
 m.setMotorSpeed(-255)
-time.sleep(5)
+time.sleep(1)
 print "stop"
 m.run(dw_MotorCONTROL.RELEASE)
