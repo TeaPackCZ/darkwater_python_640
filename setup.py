fd9c62fc6a9f891faf8faec695bbf8a8c826139e
shrkey <barry@shrkey.com>
2016-10-04 21:58:07 +0100

    quick code test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name              = 'darkwater_640',
+      version           = '1.0.0',
+      author            = 'Team Dark Water',
+      author_email      = 'team@darkwater.io',
+      description       = 'Library for Dark Water 640 board',
+      license           = 'MIT',
+      url               = 'https://github.com/darkwaterfoundation/darkwater_python_640',
+      packages          = ['darkwater_640']
+      )
