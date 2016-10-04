f3bb28e5764872f57d9f6aa1019444c52c4763fc
shrkey <barry@shrkey.com>
2016-10-04 22:48:21 +0100

    messages on stepper example

--- examples/640steppertest.py
+++ examples/640steppertest.py
@@ -14,20 +14,18 @@ s1.setMotorSpeed(200)
 s2.setMotorSpeed(200)
 s3.setMotorSpeed(200)
 
+print("1 Forward")
 s1.step( 400, dw_Controller.FORWARD, dw_Controller.DOUBLE )
+print("2 Forward")
 s2.step( 400, dw_Controller.FORWARD, dw_Controller.DOUBLE )
+print("3 Forward")
 s3.step( 400, dw_Controller.FORWARD, dw_Controller.DOUBLE )
 
+print("1 Reverse")
 s1.step( 400, dw_Controller.REVERSE, dw_Controller.DOUBLE )
+print("2 Reverse")
 s2.step( 400, dw_Controller.REVERSE, dw_Controller.DOUBLE )
-s3.step( 400, dw_Controller.REVERSE, dw_Controller.DOUBLE )
-
-s1.step( 400, dw_Controller.FORWARD, dw_Controller.DOUBLE )
-s2.step( 400, dw_Controller.FORWARD, dw_Controller.DOUBLE )
-s3.step( 400, dw_Controller.FORWARD, dw_Controller.DOUBLE )
-
-s1.step( 400, dw_Controller.REVERSE, dw_Controller.DOUBLE )
-s2.step( 400, dw_Controller.REVERSE, dw_Controller.DOUBLE )
+print("3 Reverse")
 s3.step( 400, dw_Controller.REVERSE, dw_Controller.DOUBLE )
 
 s1.off()
